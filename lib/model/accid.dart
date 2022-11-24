fd115f3a44fc17c748572e95ec4bc3d75919f738
kimchangwoo <qwertyuiop4m@gmail.com>
2022-11-24 15:58:29 +0900

    a

--- lib/model/accid.dart
+++ lib/model/accid.dart
@@ -1,14 +1,30 @@
+//class Accident{
+//  late String title;
+//  late String description;
+
+//  Accident({
+//    required this.title,
+//    required this.description
+//  });
+
+//  Accident.fromMap(Map<String, dynamic>? map) {
+//    title = map?['title'] ?? '';
+//    description = map?['description'] ?? '';
+//  }
+//}
 class Accident{
-  late String title;
-  late String description;
+  String? title;
+  String? description;
 
   Accident({
-    required this.title,
-    required this.description
+    this.title,
+    this.description
   });
 
-  Accident.fromMap(Map<String, dynamic>? map) {
-    title = map?['title'] ?? '';
-    description = map?['description'] ?? '';
+  factory Accident.fromJson(Map<String, dynamic> jsonData){
+    return Accident(
+        title:jsonData['title'],
+        description: jsonData['description']
+    );
   }
 }
--- lib/provider/providers.dart
+++ lib/provider/providers.dart
@@ -5,20 +5,14 @@ import 'package:http/http.dart' as http;
 
 class AccidentProviders{
   Uri uri = Uri.parse("http://localhost:8080/accident");
-
   Future<List<Accident>> getAccident() async {
     List<Accident> accident = [];
-
     final response = await http.get(uri);
-
     if (response.statusCode == 200) {
       accident = jsonDecode(response.body)['articles'].map<Accident>( (article) {
-        return Accident.fromMap(article);
+        return Accident.fromJson(article);
       }).toList();
     }
-
     return accident;
   }
-
-
 }
